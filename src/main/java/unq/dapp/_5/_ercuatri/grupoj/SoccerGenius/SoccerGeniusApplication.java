a29ceba39fd344ce7f68d147a7096a677b2cd203
AcoMat <aaco.maty@gmail.com>
2025-04-02 19:59:02 -0300

    Added swagger dependency

--- src/main/java/unq/dapp/_5/_ercuatri/grupoj/SoccerGenius/SoccerGeniusApplication.java
+++ src/main/java/unq/dapp/_5/_ercuatri/grupoj/SoccerGenius/SoccerGeniusApplication.java
@@ -2,8 +2,9 @@ package unq.dapp._5._ercuatri.grupoj.SoccerGenius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SoccerGeniusApplication {
 
 	public static void main(String[] args) {
