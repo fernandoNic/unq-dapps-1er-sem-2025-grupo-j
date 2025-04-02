a95ec71aef17acef0ce6d020fcce874e34b1fd90
AcoMat <aaco.maty@gmail.com>
2025-04-02 19:36:04 -0300

    Spring boot initializer

---
+++ src/main/java/unq/dapp/_5/_ercuatri/grupoj/SoccerGenius/SoccerGeniusApplication.java
@@ -0,0 +1,13 @@
+package unq.dapp._5._ercuatri.grupoj.SoccerGenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerGeniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerGeniusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unq/dapp/_5/_ercuatri/grupoj/SoccerGenius/SoccerGeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package unq.dapp._5._ercuatri.grupoj.SoccerGenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerGeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
