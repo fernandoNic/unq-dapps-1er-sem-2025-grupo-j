a369403e5978ea8b3784a4dcd7b94fc9c530c5f4
AcoMat <aaco.maty@gmail.com>
2025-04-07 19:48:13 -0300

    FIX: update swagger dependency

--- src/main/java/unq/dapp/_5/_ercuatri/grupoj/SoccerGenius/controllers/AuthController.java
+++ src/main/java/unq/dapp/_5/_ercuatri/grupoj/SoccerGenius/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package unq.dapp._5._ercuatri.grupoj.SoccerGenius.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +18,14 @@ public class AuthController {
     @Autowired private AuthService authService;
 
     @PostMapping("/register")
+    @Operation(summary = "Register a new user and return a JWT token")
     public ResponseEntity<UserDTO> register(@Valid @RequestBody RegisterFormDTO data) {
         AuthResponse new_user = authService.register(data);
         return ResponseEntity.ok().header("Authorization", "Bearer " + new_user.getToken()).body(new_user.getUser());
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Login a user and return a JWT token")
     public ResponseEntity<UserDTO> login(@Valid @RequestBody LoginCredentials credentials) {
         AuthResponse logged_user = authService.login(credentials);
         return ResponseEntity.ok().header("Authorization", "Bearer " + logged_user.getToken()).body(logged_user.getUser());
