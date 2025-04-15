33b0bbdd31b94c7dbdb34113e16000f764a90c50
AcoMat <aaco.maty@gmail.com>
2025-04-15 16:08:12 -0300

    missing validation

--- src/main/java/unq/dapp/grupoj/soccergenius/model/dtos/LoginCredentials.java
+++ src/main/java/unq/dapp/grupoj/soccergenius/model/dtos/LoginCredentials.java
@@ -1,12 +1,16 @@
 package unq.dapp.grupoj.soccergenius.model.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class LoginCredentials {
+    @Email(message = "Email is not valid")
     private String email;
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 }
 
